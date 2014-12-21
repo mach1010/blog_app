9b9bb3f6d4dbd76d7d9dbd0f8ad599bfb77477fc
mach1010 <jetstream1010@gmail.com>
2014-12-20 23:22:32 -0600

    dynamic page titles

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,18 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Mach1010 blog app"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Mach1010 blog app"
   end
 
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "About | Mach1010 blog app"
+  end
 end
