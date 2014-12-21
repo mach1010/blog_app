045db695b113e21e692ae3b35d6401fa5d5be632
mach1010 <jetstream1010@gmail.com>
2014-12-20 23:47:03 -0600

    refactor static-pages controller test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,21 +1,26 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  
+  def setup
+    @base_title = "| Mach1010 blog app"
+  end
+  
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Mach1010 blog app"
+    assert_select "title", "Home #{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Mach1010 blog app"
+    assert_select "title", "Help #{@base_title}"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | Mach1010 blog app"
+    assert_select "title", "About #{@base_title}"
   end
 end
