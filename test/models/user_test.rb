f1567299653b97caa2b05a29824e9112d7c4a1d1
mach1010 <jetstream1010@gmail.com>
2014-12-23 15:40:25 -0600

    ch 6 exercises added

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -40,7 +40,7 @@ class UserTest < ActiveSupport::TestCase
   end
   
   test "email validation should reject invalid addresses" do
-    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example. foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example. foo@bar_baz.com foo@bar+baz.com foo@bar..com]
     invalid_addresses.each do |invalid_address|
       @user.email = invalid_address
       assert_not @user.valid?, "#{invalid_address} should be invalid"
@@ -57,5 +57,11 @@ class UserTest < ActiveSupport::TestCase
   test "password should have a minimum length" do
     @user.password = @user.password_confirmation = 'a' * 5
     assert_not @user.valid?
+  end
+  
+  test "email addresses should be downcased" do
+    @user.email = @user.email.upcase
+    @user.save
+    assert_equal @user.reload.email, @user.email.downcase
   end
 end
