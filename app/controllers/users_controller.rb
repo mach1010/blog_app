489bf598856299c6c21d1396f79aeb67d8c5c2d1
mach1010 <jetstream1010@gmail.com>
2014-12-25 17:39:08 -0600

    add shared error messages partial and implement error handling with invalid info in signup form. Add integration test to verify invalid info does not create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,21 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      #render user_path(params[:id])
+    else
+      render 'new'
+    end
+  end
+  
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+  
 end
