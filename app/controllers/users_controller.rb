08fede9fa3b2dbdc472508e04490aec81dc049d8
mach1010 <jetstream1010@gmail.com>
2014-12-26 16:06:48 -0600

    finish signup, add valid signup integration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      #render user_path(params[:id])
+      flash[:success] = "Welcome to the Blog_app!"
+      redirect_to @user
     else
       render 'new'
     end
