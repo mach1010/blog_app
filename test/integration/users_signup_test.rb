25936549ecd811bc6518fbcbd1e1f9b09607385c
mach1010 <jetstream1010@gmail.com>
2014-12-27 11:29:46 -0600

    user test and add content_tag in application.html.erb

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -8,6 +8,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       post users_path, user: { name: '', email: "user@invalid", password: "foo", password_confirmation: "bar" }
     end
     assert_template 'users/new'
+    assert_select "div.alert-danger"
+    assert_select "div#error_explanation"
   end
   
   test "valid signup information" do
@@ -19,5 +21,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                             password_confirmation: "password" }
     end
     assert_template 'users/show' 
+    assert_not flash.nil?
   end 
+  
 end
