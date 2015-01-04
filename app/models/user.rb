122f57ba9a1851c236cb90e6e95d1ddb786c0828
mach1010 <jetstream1010@gmail.com>
2015-01-04 08:01:54 -0600

    move methods out of private in user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -57,18 +57,19 @@ class User < ActiveRecord::Base
     update_attribute(:reset_digest, User.digest(reset_token))
     update_attribute(:reset_sent_at, Time.zone.now)
   end
-  
-  private
-  def downcase_email
-    self.email = email.downcase
-  end
-  
+
   def create_activation_digest
     self.activation_token = User.new_token
     self.activation_digest = User.digest(activation_token)
-  end
+  end  
 
   def send_password_reset_email
     UserMailer.password_reset(self).deliver_now
   end
+
+  private
+  def downcase_email
+    self.email = email.downcase
+  end
+
 end
